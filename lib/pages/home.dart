4254038c57b6185f9b924fe7dfbf5b9a44631c55
lbarsidati22 <lbarsidati575@gmail.com>
2024-09-21 22:39:05 +0000

    create drawer

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,50 +6,114 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          actions: [
-            Stack(
+    return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
               children: [
-                Container(
-                  padding: EdgeInsets.all(4),
-                  child: Text(
-                    '8',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400,
+                UserAccountsDrawerHeader(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/backgroundimage.jpg'),
+                      fit: BoxFit.cover,
                     ),
                   ),
-                  decoration: BoxDecoration(
-                    color: Colors.green,
-                    shape: BoxShape.circle,
+                  currentAccountPicture: CircleAvatar(
+                    radius: 55,
+                    backgroundImage: AssetImage("assets/images/lbar.jpg"),
+                  ),
+                  accountName: Text(
+                    'Lbar sidati',
+                  ),
+                  accountEmail: Text(
+                    'lbar@gmail.com',
                   ),
                 ),
-                IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      color: Colors.white,
-                      Icons.add_shopping_cart,
-                    )),
+                ListTile(
+                  title: Text('Home'),
+                  leading: Icon(Icons.home),
+                  onTap: () {},
+                ),
+                ListTile(
+                  title: Text('My Prudact'),
+                  leading: Icon(Icons.add_shopping_cart),
+                  onTap: () {},
+                ),
+                ListTile(
+                  title: Text('about'),
+                  leading: Icon(
+                    Icons.help_center,
+                  ),
+                  onTap: () {},
+                ),
+                ListTile(
+                  title: Text('logout'),
+                  leading: Icon(
+                    Icons.exit_to_app,
+                  ),
+                  onTap: () {},
+                ),
               ],
             ),
             Padding(
-              padding: const EdgeInsets.only(
-                right: 12,
-              ),
+              padding: const EdgeInsets.all(8.0),
               child: Text(
-                '\$103',
-                style: TextStyle(color: Colors.white),
+                'Developed by lbar sidati 2024',
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w600,
+                ),
               ),
             ),
           ],
-          backgroundColor: appbarGreen,
-          title: Text(
-            'Home',
-            style: TextStyle(color: Colors.white),
+        ),
+      ),
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: Colors.white,
+        ),
+        actions: [
+          Stack(
+            children: [
+              Container(
+                padding: EdgeInsets.all(4),
+                child: Text(
+                  '8',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  shape: BoxShape.circle,
+                ),
+              ),
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    color: Colors.white,
+                    Icons.add_shopping_cart,
+                  )),
+            ],
           ),
+          Padding(
+            padding: const EdgeInsets.only(
+              right: 12,
+            ),
+            child: Text(
+              '\$103',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+        backgroundColor: appbarGreen,
+        title: Text(
+          'Home',
+          style: TextStyle(color: Colors.white),
         ),
       ),
     );
