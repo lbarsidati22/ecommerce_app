a5181eab16ce2161a3481c0dd2c0bb83e48a0f89
lbarsidati22 <lbarsidati575@gmail.com>
2024-10-04 22:19:47 +0000

    edit data done

--- lib/pages/Profile_page.dart
+++ lib/pages/Profile_page.dart
@@ -51,75 +51,77 @@ class _ProfilePageState extends State<ProfilePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: Container(
-                padding: EdgeInsets.all(11),
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(11),
-                ),
-                child: Text(
-                  'info form firebase auth',
-                  style: TextStyle(
-                    fontSize: 21,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Container(
+                  padding: EdgeInsets.all(11),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(11),
+                  ),
+                  child: Text(
+                    'info form firebase auth',
+                    style: TextStyle(
+                      fontSize: 21,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Email : ${credential!.email}',
-                  style: TextStyle(
-                    fontSize: 17,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Email : ${credential!.email}',
+                    style: TextStyle(
+                      fontSize: 17,
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 11,
-                ),
-                Text(
-                  'Created date : ${DateFormat("MMM d, y").format(credential.metadata.creationTime!)}',
-                  style: TextStyle(
-                    fontSize: 17,
+                  SizedBox(
+                    height: 11,
                   ),
-                ),
-                SizedBox(
-                  height: 11,
-                ),
-                Text(
-                  'Last signed in :  ${DateFormat("MMM d, y").format(credential.metadata.lastSignInTime!)}',
-                  style: TextStyle(
-                    fontSize: 17,
+                  Text(
+                    'Created date : ${DateFormat("MMM d, y").format(credential.metadata.creationTime!)}',
+                    style: TextStyle(
+                      fontSize: 17,
+                    ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            Center(
-              child: Container(
-                padding: EdgeInsets.all(11),
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(11),
-                ),
-                child: Text(
-                  'info form firebase firestor',
-                  style: TextStyle(
-                    fontSize: 21,
+                  SizedBox(
+                    height: 11,
+                  ),
+                  Text(
+                    'Last signed in :  ${DateFormat("MMM d, y").format(credential.metadata.lastSignInTime!)}',
+                    style: TextStyle(
+                      fontSize: 17,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Center(
+                child: Container(
+                  padding: EdgeInsets.all(11),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(11),
+                  ),
+                  child: Text(
+                    'info form firebase firestor',
+                    style: TextStyle(
+                      fontSize: 21,
+                    ),
                   ),
                 ),
               ),
-            ),
-            GetDataFirestor(
-              documentId: credential.uid,
-            ),
-          ],
+              GetDataFirestor(
+                documentId: credential.uid,
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/shared/data_firestore.dart
+++ lib/shared/data_firestore.dart
@@ -1,17 +1,86 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class GetDataFirestor extends StatelessWidget {
+class GetDataFirestor extends StatefulWidget {
   final String documentId;
 
   const GetDataFirestor({super.key, required this.documentId});
+
+  @override
+  State<GetDataFirestor> createState() => _GetDataFirestorState();
+}
+
+final dilogUserController = TextEditingController();
+final credential = FirebaseAuth.instance.currentUser;
+CollectionReference users = FirebaseFirestore.instance.collection('userss');
+
+class _GetDataFirestorState extends State<GetDataFirestor> {
+  myShowDialog(Map data, dynamic myKey) {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(11)),
+          child: Container(
+            padding: EdgeInsets.all(22),
+            height: 200,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                    controller: dilogUserController,
+                    maxLength: 20,
+                    decoration: InputDecoration(hintText: "${data[myKey]}")),
+                SizedBox(
+                  height: 22,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    TextButton(
+                        onPressed: () {
+                          // addnewtask();
+                          users
+                              .doc(credential!.uid)
+                              .update({myKey: dilogUserController.text});
+
+                          setState(() {
+                            Navigator.pop(context);
+                          });
+                        },
+                        child: Text(
+                          "Edit",
+                          style: TextStyle(fontSize: 18),
+                        )),
+                    TextButton(
+                        onPressed: () {
+                          // addnewtask();
+                          Navigator.pop(context);
+                        },
+                        child: Text(
+                          "Cancel",
+                          style: TextStyle(fontSize: 18),
+                        )),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     CollectionReference users = FirebaseFirestore.instance.collection('userss');
 
     return FutureBuilder<DocumentSnapshot>(
-      future: users.doc(documentId).get(),
+      future: users.doc(widget.documentId).get(),
       builder:
           (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
         if (snapshot.hasError) {
@@ -26,55 +95,119 @@ class GetDataFirestor extends StatelessWidget {
           Map<String, dynamic> data =
               snapshot.data!.data() as Map<String, dynamic>;
 
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 13,
-              ),
-              Text(
-                "username: ${data['username']} ",
-                style: TextStyle(
-                  fontSize: 18,
+          return SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 13,
                 ),
-              ),
-              SizedBox(
-                height: 13,
-              ),
-              Text(
-                "age: ${data['age']} ",
-                style: TextStyle(
-                  fontSize: 18,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "username: ${data['username']} ",
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        myShowDialog(data, 'username');
+                      },
+                      icon: Icon(
+                        Icons.edit,
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              SizedBox(
-                height: 13,
-              ),
-              Text(
-                "title: ${data['title']} ",
-                style: TextStyle(
-                  fontSize: 18,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "age: ${data['age']} ",
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        myShowDialog(data, 'age');
+                      },
+                      icon: Icon(
+                        Icons.edit,
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              SizedBox(
-                height: 13,
-              ),
-              Text(
-                "email: ${data['email']} ",
-                style: TextStyle(
-                  fontSize: 18,
+                SizedBox(
+                  height: 13,
                 ),
-              ),
-              SizedBox(
-                height: 13,
-              ),
-              Text(
-                "password: ${data['pass']} ",
-                style: TextStyle(
-                  fontSize: 18,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "title: ${data['title']} ",
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        myShowDialog(data, 'title');
+                      },
+                      icon: Icon(
+                        Icons.edit,
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: 13,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "email: ${data['email']} ",
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        myShowDialog(data, 'email');
+                      },
+                      icon: Icon(
+                        Icons.edit,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 13,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "password: ${data['pass']} ",
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        myShowDialog(data, 'pass');
+                      },
+                      icon: Icon(
+                        Icons.edit,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           );
         }
 
